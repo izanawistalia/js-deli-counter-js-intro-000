dcadfd3a24dc06e6d0c0bc65096bffc3eebb7148
rihan <dikshabelwal0@gmail.com>
2019-05-31 05:48:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function takeANumber( arr, name){
   let i=0;
   arr.push(name);
   i=arr.length;
-  return `"Welcome, ${name}. You are number ${i} in line."`;
+  return `Welcome, ${name}. You are number ${i} in line.`;
 
 }
