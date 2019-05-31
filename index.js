d86c954e4a901ec662dfa40a5f53c779fcac081f
rihan <dikshabelwal0@gmail.com>
2019-05-31 05:47:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 function takeANumber( arr, name){
-
-  for(let i=0; i<arr.length ; ++i ){
-      if(name === arr[i]){
-         var sent = `"Welcome, ${name}. You are number ${i+1} in line."`;
-         return sent;
-  }
-  else{
-    continue;
-  }
-  console.log("doesnot exist");
-}
+  let i=0;
+  arr.push(name);
+  i=arr.length;
+  return `"Welcome, ${name}. You are number ${i} in line."`;
 
 }
